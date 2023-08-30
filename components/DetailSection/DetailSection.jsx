3a1ea7130d8793f2fd9bcc338c79c8a939717e81
LucasRibotta <lucasribotta15@gmail.com>
2023-08-30 17:34:57 -0300

    update detail

--- components/DetailSection/DetailSection.jsx
+++ components/DetailSection/DetailSection.jsx
@@ -1,62 +1,62 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect } from 'react'
-import { db } from '../../firebaseConfig'
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebaseConfig';
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+
+function BitacoraDetail({ bitacora }) {
+  return (
+    <div>
+      <h1>{bitacora.title}</h1>
+      <p>{bitacora.description}</p>
+      <p>{bitacora.culture}</p>
+      <div>
+        <h2>{bitacora.notes}</h2>
+        <h2>{bitacora.activitys}</h2>
+      </div>
+      <div>
+        <img src={bitacora.image} alt="img" width={70} height={100} />
+      </div>
+      <div>
+        <button onClick={() => deleteBitacora(bitacora.id)}>Eliminar Bitacora</button>
+        <button>Editar Bitacora</button>
+      </div>
+    </div>
+  );
+}
 
 export default function DetailSection() {
-  const colors = ['default']
+  const [bitacoraList, setBitacoraList] = useState([]);
 
-  const [bitacoraList, setBitacoraList] = useState([])
-
-  const bitacoraColecctionRef = collection(db, "bitacoras");
+  const bitacoraCollectionRef = collection(db, "bitacoras");
 
   useEffect(() => {
-    const getBitacroaList = async () => {
+    const getBitacoraList = async () => {
       try {
-        const dataBitacora = await getDocs(bitacoraColecctionRef)
+        const dataBitacora = await getDocs(bitacoraCollectionRef);
         const filteredData = dataBitacora.docs.map(doc => ({
           ...doc.data(),
-          id: doc.id
-        }))
-        setBitacoraList(filteredData)
+          id: doc.id,
+        }));
+        setBitacoraList(filteredData);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-    getBitacroaList()
-  }, [])
+    };
+    getBitacoraList();
+  }, []);
 
   const deleteBitacora = async (id) => {
-    const bitacoraDoc = doc(db, "bitacoras", id)
-    await deleteDoc(bitacoraDoc)
-  }
+    const bitacoraDoc = doc(db, "bitacoras", id);
+    await deleteDoc(bitacoraDoc);
+    setBitacoraList(bitacoraList.filter(bitacora => bitacora.id !== id));
+  };
 
   return (
-    <div className='flex flex-wrap gap-4 h-screen'>
+    <div className="flex flex-wrap gap-4 h-screen">
       {bitacoraList.map((bitacora) => (
-        <div key={bitacora.id}>
-          <h1>{bitacora.title}</h1>
-          <p>{bitacora.description}</p>
-          <p>{bitacora.culture}</p>
-          <div>
-            <h2>{bitacora.notes}</h2>
-            <h2>{bitacora.activitys}</h2>
-          </div>
-
-          <div>
-            <img  src={bitacora.image} alt="img" width={30} height={50}/>
-          </div>
-
-          <div>
-            <button onClick={deleteBitacora(bitacora.id)}>Eliminar Bitacora</button>
-            <button>Editar Bitacora</button>
-          </div>
-        </div>
-
+        <BitacoraDetail key={bitacora.id} bitacora={bitacora} />
       ))}
-
-
     </div>
-  )
+  );
 }
