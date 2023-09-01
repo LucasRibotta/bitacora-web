75787316526beeed9f490597a021aab0d90801b4
LucasRibotta <lucasribotta15@gmail.com>
2023-09-01 01:29:50 -0300

    correction detail section

--- components/DetailSection/DetailSection.jsx
+++ components/DetailSection/DetailSection.jsx
@@ -1,29 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 import { db } from '../../firebaseConfig';
-import { useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
 import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
 
+
 export default function DetailSection() {
-  const [bitacoraList, setBitacoraList] = useState([]);
-  const router = useRouter()
+  const [data, setData] = useState()
+  const params = useParams()
 
-  const deleteBitacora = async (id) => {
+  const info = data && data.find(el => el.id === params.id)
+
+
+  const getBitacora = async (id) => {
     try {
-      const bitacoraDoc = doc(db, "bitacoras", id);
-      await deleteDoc(bitacoraDoc);
+      const bitacoraDoc = await doc(db, "bitacoras", params.id);
+      setData(bitacoraDoc)
 
-      setBitacoraList(prevBitacoras =>
-        prevBitacoras.filter(item => item.id !== id)
-      );
-
-      alert("Bitácora eliminada con éxito");
-      router.push('/home')
     } catch (error) {
-      console.error("Error al eliminar la bitácora:", error);
+      alert('Error en la bitacora')
     }
-  };
+  }
 
   const bitacoraCollectionRef = collection(db, "bitacoras");
 
@@ -35,7 +33,7 @@ export default function DetailSection() {
           ...doc.data(),
           id: doc.id,
         }));
-        setBitacoraList(filteredData);
+        setData(filteredData);
       } catch (error) {
         console.error(error);
       }
@@ -43,22 +41,23 @@ export default function DetailSection() {
     getBitacoraList();
   }, []);
 
+
   return (
     <div className="flex flex-wrap gap-4 min-h-screen">
-      {bitacoraList.map((bitacora) => (
+
         <div
-          key={bitacora.id}
+          key={info.id}
           className="bg-slate-50 border p-4 rounded-md shadow-md justify-center h-screen w-auto m-auto"
         >
-          <h1 className="text-xl text-green-600 font-bold mb-2">{bitacora.title}</h1>
-          <p className="text-lime-500 font-semibold mb-2">{bitacora.description}</p>
-          <p className="text-lime-700 font-semibold mb-4">{bitacora.culture}</p>
+          <h1 className="text-xl text-green-600 font-bold mb-2">{info.title}</h1>
+          <p className="text-lime-500 font-semibold mb-2">{info.description}</p>
+          <p className="text-lime-700 font-semibold mb-4">{info.culture}</p>
           <div className="mb-4">
             <h2 className="font-semibold mb-1 underline">Notas:</h2>
-            <p>{bitacora.notes}</p>
+            <p>{info.notes}</p>
             <h2 className="font-semibold my-3 underline">Actividades:</h2>
             <div className="grid grid-cols-2 gap-2">
-              {bitacora.activitys.map((activity, index) => (
+              {info.activitys.map((activity, index) => (
                 <p key={index} className="text-gray-600">
                   {activity}
                 </p>
@@ -66,7 +65,7 @@ export default function DetailSection() {
             </div>
           </div>
           <div className="grid grid-cols-4 gap-4 justify-center">
-            {bitacora.image.map((imageUrl, index) => (
+            {info.image.map((imageUrl, index) => (
               <img
                 key={index}
                 src={imageUrl}
@@ -84,38 +83,7 @@ export default function DetailSection() {
             </button>
           </div>
         </div>
-      ))}
     </div>
   );
 } 
 
-/* import React, {useState} from 'react'
-import { db } from '../../firebaseConfig';
-import { useParams, usePathname, useRouter } from 'next/navigation'
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
-
-
-export default function DetailSection() {
-  const [data, setData] = useState()
-  const params = useParams()
-
-  const getBitacora = async (id) => {
-    try {
-      const bitacoraDoc = await doc(db, "bitacoras", id);
-      setData(bitacoraDoc)
-
-    } catch (error) {
-      alert('Error en la bitacora')
-    }
-  }
-
-
-
-
-  return (
-    <div>
-
-
-    </div>
-  )
-} */
