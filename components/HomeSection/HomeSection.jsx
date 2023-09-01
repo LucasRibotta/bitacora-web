f56abd5ee17f47c415fa8f6eccf1c7a10d949646
LucasRibotta <lucasribotta15@gmail.com>
2023-09-01 02:40:04 -0300

    loading estructure en homeSection

--- components/HomeSection/HomeSection.jsx
+++ components/HomeSection/HomeSection.jsx
@@ -5,46 +5,45 @@ import { db, auth } from '../../firebaseConfig';
 import { getDocs, collection, query, where } from 'firebase/firestore';
 import CardHome from '../Card/Card';
 import { Button } from '@nextui-org/react';
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 export default function HomeSection() {
   const [bitacoraList, setBitacoraList] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [user, loading, error] = useAuthState(auth)
 
   const bitacoraCollectionRef = collection(db, 'bitacoras');
 
-  useEffect(() => {
-    const getBitacoraList = async () => {
-      try {
-        if (!auth.currentUser) {
-          console.log("Usuario no autenticado.");
-          return;
-        }
 
-        const q = query(
-          bitacoraCollectionRef,
-          where('userId', '==', auth.currentUser.uid)
-        );
-
-        console.log("Consulta:", q); 
-
-        const dataBitacora = await getDocs(q);
-        const filteredData = dataBitacora.docs.map(doc => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-
-        console.log("Bitácoras filtradas:", filteredData); 
-
-        setBitacoraList(filteredData);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
+  const getBitacoraList = async () => {
+    try {
+      if (!auth.currentUser) {
+        alert("Usuario no autenticado.");
+        return;
       }
-    };
 
+      const q = query(
+        bitacoraCollectionRef,
+        where('userId', '==', auth.currentUser.uid)
+      );
+
+      console.log("Consulta:", q);
+
+      const dataBitacora = await getDocs(q);
+      const filteredData = dataBitacora.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      console.log("Bitácoras filtradas:", filteredData);
+
+      setBitacoraList(filteredData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
     getBitacoraList();
-  }, []);
+  }, [loading]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 h-screen p-4 bg-slate-50">
